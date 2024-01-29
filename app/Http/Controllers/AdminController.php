bca803910f8117a3d4717af385c38f184e431437
HannoBurger <burgerhanno@gmail.com>
2024-01-29 21:43:10 +0200

    Test

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Message;
-use App\Models\Vacancy; 
+use App\Models\Vacancy;
 use App\Models\Position;
 use App\Models\Applicant;
 use App\Models\Application;
