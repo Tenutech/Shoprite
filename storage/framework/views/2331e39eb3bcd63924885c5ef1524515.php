edb09e361bcaa49c1abe8a01cc58f30c6f037bd1
Shadley Wentzel <shadley@turfsport.co.za>
2024-05-09 09:53:42 +0200

    Fixed views

--- storage/framework/views/2331e39eb3bcd63924885c5ef1524515.php
+++
@@ -1,4 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> --><?php /**PATH /Users/shadley/Development/Sourcecode/tenubah/Orient/resources/views/layouts/body.blade.php ENDPATH**/ ?>
--- storage/framework/views/8e58d3ea43591e43e7864e8463e6ee6b.php
+++
@@ -1,4 +0,0 @@
-
-<body>
-
-<!-- <body data-layout="horizontal"> --><?php /**PATH C:\xampp\htdocs\Orient\resources\views/layouts/body.blade.php ENDPATH**/ ?>
