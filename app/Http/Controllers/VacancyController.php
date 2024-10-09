f6f8ab8fa27d65c90a2c03a40c3319166b4d7eef
HannoBurger <burgerhanno@gmail.com>
2024-10-09 09:02:37 +0200

    Changed Confirm to Confirmed

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -670,7 +670,7 @@ class VacancyController extends Controller
                     $interview = Interview::where('applicant_id', $applicant->id)
                         ->where('vacancy_id', $vacancy->id)
                         ->where('interviewer_id', Auth::id())
-                        ->whereIn('status', ['Scheduled', 'Reschedule', 'Confirm', 'Completed'])
+                        ->whereIn('status', ['Scheduled', 'Reschedule', 'Confirmed', 'Completed'])
                         ->first();
 
                     // If application exists, update its approval status to 'No'
