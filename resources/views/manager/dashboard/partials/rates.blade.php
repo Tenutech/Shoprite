7e54653108a21caa5d3812720d1d8451a0e131c6
Shadley <shadleywentzel@gmail.com>
2024-10-11 15:48:13 +0200

    SS-158 | Manager dashboard cleanup

---
+++ resources/views/manager/dashboard/partials/rates.blade.php
@@ -0,0 +1,128 @@
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card crm-widget">
+            <div class="card-body p-0">
+                <div class="row row-cols-lg-3 row-cols-md-3 row-cols-1 g-0">
+                    <div class="col">
+                        <div class="py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Time to Shortlist (days)</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-space-ship-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0"><span class="counter-value" data-target="{{ $storeAverageTimeToShortlist }}">{{ $storeAverageTimeToShortlist }}</span>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                    <div class="col">
+                        <div class="mt-3 mt-md-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Time to Hire (days)</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0"><span class="counter-value"
+                                            data-target="{{ $storeAverageTimeToHire }}">{{ $storeAverageTimeToHire }}</span></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                    <div class="col">
+                        <div class="mt-3 mt-md-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Adoption Rate </h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-pulse-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0"><span class="counter-value"
+                                            data-target="{{ $adoptionRate }}">{{ $adoptionRate }}</span>%</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end card body -->
+        </div><!-- end card -->
+    </div><!-- end col -->
+</div><!-- end row -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card crm-widget">
+            <div class="card-body p-0">
+                <div class="row row-cols-lg-4 row-cols-md-3 row-cols-1 g-0">
+                    <div class="col">
+                        <div class="mt-3 mt-lg-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Average Store Literacy Score</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-trophy-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0">
+                                        <span class="counter-value" data-target="{{ $adoptionRate }}">{{ $averageScores['avg_literacy_score'] }}</span>%
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                    
+                    <div class="col">
+                        <div class="mt-3 mt-lg-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Average Store Numeracy Score</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-service-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0">
+                                        <span class="counter-value" data-target="{{ $averageScores['avg_numeracy_score'] }}" id="averageDistanceSuccessfulPlacementsValue">
+                                        {{ $averageScores['avg_numeracy_score'] }}
+                                        </span>%
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                    
+                    <div class="col">
+                        <div class="mt-3 mt-md-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Average Distance for Successful Placements</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0">
+                                        <span class="counter-value" data-target="{{ $averageTalentPoolDistance }}">{{ $averageDistanceSuccessfulPlacements }}</span>km
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                    
+                    <div class="col">
+                        <div class="mt-3 mt-md-0 py-4 px-3">
+                            <h5 class="text-muted text-uppercase fs-13">Average Distance for All Applicants</h5>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h2 class="mb-0">
+                                        <span class="counter-value" data-target="{{ $averageTalentPoolDistance }}">{{ $averageTalentPoolDistance }}</span>km
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end card body -->
+        </div><!-- end card -->
+    </div><!-- end col -->
+</div><!-- end row -->
