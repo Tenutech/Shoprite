0491d9e4ac331aa7dc1d4d04b16b6fe12c997c1f
HannoBurger <burgerhanno@gmail.com>
2024-10-15 09:27:28 +0200

    Changed Menu Items

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -98,19 +98,19 @@
                     <li class="nav-item">
                         <a class="nav-link menu-link" href="{{ route('shortlist.index') }}">
                             <i class="ri-list-check-2"></i> 
-                            <span>Shortlist</span>
+                            <span>My Shortlists</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="{{ route('interviews.index') }}">
+                            <i class="ri-briefcase-line"></i> 
+                            <span>My Interviews</span>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link menu-link" href="{{ route('applicants.index') }}">
                             <i class="ri-profile-line"></i> 
                             <span>Saved Candidates</span>
-                        </a>
-                    </li>                    
-                    <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{ route('interviews.index') }}">
-                            <i class="ri-briefcase-line"></i> 
-                            <span>Interviews</span>
                         </a>
                     </li>
                 @endif
