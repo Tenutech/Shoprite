0fc9a10d2401193b9c8d79e3b9850769f8ffa8be
HannoBurger <burgerhanno@gmail.com>
2025-01-21 21:37:51 +0200

    Migration Fix

--- database/migrations/2025_01_21_143524_add_code5_and_code6_to_stores_table.php
+++ database/migrations/2025_01_21_143524_add_code5_and_code6_to_stores_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,15 +14,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('stores', function (Blueprint $table) {
-            if (!Schema::hasColumn('stores', 'code_5')) {
-                $table->string('code_5', 50)->nullable()->after('code');
-            }
+        if (!Schema::hasColumn('stores', 'code_5')) {
+            DB::statement("ALTER TABLE `stores` ADD `code_5` VARCHAR(50) NULL AFTER `code`");
+        }
 
-            if (!Schema::hasColumn('stores', 'code_6')) {
-                $table->string('code_6', 50)->nullable()->after('code_5');
-            }
-        });
+        if (!Schema::hasColumn('stores', 'code_6')) {
+            DB::statement("ALTER TABLE `stores` ADD `code_6` VARCHAR(50) NULL AFTER `code_5`");
+        }
     }
 
     /**
@@ -31,14 +30,16 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('stores', function (Blueprint $table) {
-            if (Schema::hasColumn('stores', 'code_5')) {
+        if (Schema::hasColumn('stores', 'code_5')) {
+            Schema::table('stores', function (Blueprint $table) {
                 $table->dropColumn('code_5');
-            }
+            });
+        }
 
-            if (Schema::hasColumn('stores', 'code_6')) {
+        if (Schema::hasColumn('stores', 'code_6')) {
+            Schema::table('stores', function (Blueprint $table) {
                 $table->dropColumn('code_6');
-            }
-        });
+            });
+        }
     }
 };
