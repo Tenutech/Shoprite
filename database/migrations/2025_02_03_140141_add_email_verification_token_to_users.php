1e437f230f0fc542817d5db742067a1f00c49519
HannoBurger <burgerhanno@gmail.com>
2025-02-05 15:28:19 +0200

    Migration Change

--- database/migrations/2025_02_03_140141_add_email_verification_token_to_users.php
+++ database/migrations/2025_02_03_140141_add_email_verification_token_to_users.php
@@ -11,15 +11,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'email_verification_token')) {
-                $table->string('email_verification_token')->nullable()->after('email_verified_at');
-            }
+        if (!Schema::hasColumn('users', 'email_verification_token') ||
+            !Schema::hasColumn('users', 'email_verification_expires_at')) {
+            Schema::table('users', function (Blueprint $table) {
+                if (!Schema::hasColumn('users', 'email_verification_token')) {
+                    $table->string('email_verification_token')->nullable()->after('email_verified_at');
+                }
 
-            if (!Schema::hasColumn('users', 'email_verification_expires_at')) {
-                $table->timestamp('email_verification_expires_at')->nullable()->after('email_verification_token');
-            }
-        });
+                if (!Schema::hasColumn('users', 'email_verification_expires_at')) {
+                    $table->timestamp('email_verification_expires_at')->nullable()->after('email_verification_token');
+                }
+            });
+        }
     }
 
     /**
